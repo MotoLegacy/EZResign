5b7f9b392de527cd87109c53b76a70f75e9e5a7e
Ian Bowling <hellomotolegacy@gmail.com>
2018-07-21 21:43:55 -0400

    Actual Initial Commit

---
+++ EZResign.py
@@ -0,0 +1,128 @@
+''' Python script to easily resign PlayStation 3 .PKGs and their .RAP
+    files for use with the PS3Han.
+    Created by MotoLegacy'''
+
+import os
+import sys
+import time
+import shutil
+
+
+failed = 0
+
+#for knowing what commandline arguments to run
+def getOS():
+    platforms = {
+        'linux1' : 'Linux',
+        'linux2' : 'Linux',
+        'darwin' : 'OS X',
+        'win32' : 'Windows',
+    }
+    if sys.platform not in platforms:
+        return sys.platform
+    else:
+        return platforms[sys.platform]
+
+#package all .RAP files into one super duper easy to install pkg
+def package():
+    global failed
+    count = 0
+    rappkg = 'RIF000-INSTALLER_00-0000000000000000.pkg_signed.pkg'
+
+    #notfy that a rap pkg is being generated
+    print('Generating .RAP PKG..')
+
+    #move all raps to where resigner can find them
+    for raps in os.listdir('.'):
+        if raps.endswith('.rap'):
+            count += 1
+            shutil.move(raps, 'bin/raps/{}'.format(raps))
+
+    #no rap files found
+    if count == 0:
+        print('\nERR: RAP PKG not generated! Make sure to put .rap files with EZResign.py before execution!')
+        failed += 1
+        return
+
+    #package on windows
+    if getOS() == 'Windows':
+        os.system('bin\SilentCMD bin\\resign_all.bat')
+    
+    #package on linux
+    elif getOS() == 'Linux':
+        os.system('nohup wine bin/resign_all.bat /dev/null 2>&1 &')
+
+    shutil.move('bin/{}'.format(rappkg), './output/{}'.format(rappkg))
+
+    #copy signed_act.dat to output
+    shutil.move('bin/signed_act.dat', 'output/act.dat')
+
+
+
+
+
+#sign the pkg with your idps and act.dat
+def resign(pkg):
+    global failed
+    noex = pkg.split('.')[0]
+    ex = pkg.split('.')[1]
+
+    #don't sign an already signed pkg!
+    if ex == 'pkg_signed':
+        return
+
+    #notify that pkg is being resigned
+    print('Resigning {}'.format(noex))
+
+    #resign on windows
+    if getOS() == 'Windows':
+        os.system('bin\SilentCMD bin\\ps3xploit_rifgen_edatresign.exe {}'.format(pkg))
+
+    #resign on linux
+    elif getOS() == 'Linux':
+        os.system('nohup wine bin/ps3xploit_rifgen_edatresign.exe {} /dev/null 2>&1 &'.format(pkg))
+
+    #resigner doesn't support OSX..  
+    else:
+        print('\nERR: Unsupported OS!')
+        wait = input('Press ENTER to continue')
+        quit()
+    
+    #make sure the file was created
+    try:
+        dump = open('ps3xploit_rifgen_edatresign.exe.stackdump')
+
+    #no dump = success   
+    except IOError:
+        print('{} resigned successfully!\n'.format(noex))
+        shutil.move('{}.pkg_signed.pkg'.format(noex), 'output/{}.pkg_signed.pkg'.format(noex))
+        return
+
+    dump.close()
+    print('ERR: Unable to resign {}, is the package corrupted?\n'.format(pkg))
+    failed += 1
+
+    os.remove('ps3xploit_rifgen_edatresign.exe.stackdump')
+    os.remove('{}.pkg_signed.pkg'.format(noex))
+    return
+
+
+def main():
+    global failed
+    for files in os.listdir('.'):
+        if files.endswith('.pkg'):
+            resign(files)
+
+    package()
+
+    if failed > 0:
+        print('\nResigned packages with {} error(s)'.format(failed))
+    else:
+        print('All packages signed successfully!')
+    
+    wait = input('\nPress ENTER to continue')
+    quit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.txt
@@ -0,0 +1,16 @@
+~~ EZResign by MotoLegacy ~~
+
+Prerequisites:
+	1. Python 3.X, 2.7
+	2. WINE (For Linux users, UNTESTED!)
+	3. A dumped IDPS.hex and ACT.dat (place in bin folder)
+
+Usage:
+	1. Place all .PKGs and .RAPs in the same directory as EZResign.py
+	2. Run EZResign.py with Python 3
+	3. After the script has completed, put ALL files in the output folder on a USB drive
+	4. Install the .PKGs with your PS3's Package Manager
+
+Special Thanks:
+	- esc0rtd3w, bguerville, Habib, and other members of the PS3Xploit Team
+	- stbrenner for making SilentCMD
